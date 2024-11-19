71036d4b3ac3426997b249823c7d6f1eafc227fa
Jenecil Patac <jenecilpatac@gmail.com>
2024-11-19 16:33:16 +0800

    first commit

--- server/config/sanctum.php
+++ server/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,136.239.196.178:5001,localhost:5001,::1,localhost:5004,',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,136.239.196.178:5001,localhost:5001,::1,localhost:5004,185.199.52.40:5004',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
